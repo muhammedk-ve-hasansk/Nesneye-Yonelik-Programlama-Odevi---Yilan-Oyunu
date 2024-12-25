c2d564ab824450f3c4f843ac1659883d277dbdb7
HasanServer <root.email@example.com>
2024-12-25 21:20:40 +0300

    Yılan oyunun main dosyası eklendi

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "Menu.h" // Menu sinifini dahil ediyoruz
+
+int main() {
+    Menu menu; // Menu sinifindan bir nesne turetiyoruz
+    menu.showMenu(); // Menuyu ekrana yazdiriyoruz
+    return 0;
+}
