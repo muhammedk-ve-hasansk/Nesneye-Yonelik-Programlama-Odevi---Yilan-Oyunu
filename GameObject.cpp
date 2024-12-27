5049c973cf4cdf66ee4aaf253360c088380d946c
HasanServer <root.email@example.com>
2024-12-27 14:20:01 +0300

    Yılan oyunun Oyun objesinin sınıfı eklendi

---
+++ GameObject.cpp
@@ -0,0 +1,4 @@
+#include "GameObject.h"     // GameObject sinifini dahil ediyoruz
+
+GameObject::GameObject(int x, int y) : x(x), y(y) {} //yilanin x ve y koordinatlarina deger atayan constructor
+GameObject::~GameObject() {}    //destructor
